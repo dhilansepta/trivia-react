771503c9134052c2d22cdf264578189bbbc352a0
dhilansepta <dhilan.septa80@gmail.com>
2025-04-25 10:54:34 +0700

    Initialize Project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="flex flex-col text-center h-dvh justify-center items-center">
+      <h1 className="text-3xl font-bold justify-center">Welcome to Fun Trivia</h1>
+    </div>
+  )
+}
+
+export default App
